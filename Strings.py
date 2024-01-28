861b6120c5be48947b30ea80e51f0a6b5c6d72e7
Carl Allen Torno <ctorno30@gmail.com>
2024-01-28 11:00:46 +0800

    ako muna

--- Strings.py
+++ Strings.py
@@ -1,6 +1,6 @@
 message = 'Hello World'
 multiple_line = """Hello Python grind
-para kay Dars"""
+para kay sa aking Future"""
 
 print(multiple_line)
 
@@ -20,7 +20,7 @@ message = message.replace('World', 'Universe') # needs to be assign because it c
 print(message)
 
 greeting = 'Hello'
-name = 'Dars'
+name = 'Celestia'
 
 #concatanating string
 message = greeting + ", " +name # add multiple and concatenating them using '+'
